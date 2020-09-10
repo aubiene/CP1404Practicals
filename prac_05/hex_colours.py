f176119ac82669d36976aafa9d58f7cf575c90e5
aubiene <aubiene.thomspon@my.jcu.edu.au>
2020-09-10 21:07:33 +1000

    fix up variable naming mistake

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
 """
 Hex colours in a dictionary
 """
-
 HEX_COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "antiquewhite1": "#ffefdb",
                     "antiquewhite2": "#eedfcc", "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378",
                     "aquamarine1": "#7fffd4", "coral": "#ff7f50", "coral3": "#cd5b45", "gray": "#bebebe"}
@@ -12,4 +11,4 @@ while colour != "":
         print("{} is {}".format(colour, HEX_COLOUR_CODES[colour]))
     else:
         print("Invalid colour")
-    colour_name = input("Enter a colour: ")
+    colour = input("Enter a colour: ")
