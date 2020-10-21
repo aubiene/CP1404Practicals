2e921d5cc6cbf04e5746e806598f4487efa0112d
aubiene <aubiene.thomspon@my.jcu.edu.au>
2020-10-22 09:58:16 +1000

    Add test_taxi.py program.

---
+++ prac_08/test_taxi.py
@@ -0,0 +1,14 @@
+from prac_08.taxi import Taxi
+
+
+def main():
+    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi.drive(40)
+    print(new_taxi)
+
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+
+
+main()
