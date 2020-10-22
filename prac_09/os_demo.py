819df9209c60819e6054f4f568debcef49c2177b
aubiene <aubiene.thomspon@my.jcu.edu.au>
2020-10-22 14:51:29 +1000

    Add os_demos file

--- prac_09/os_demo.py
+++ prac_09/os_demo.py
@@ -2,12 +2,11 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
 import os
 
 
 def main():
-    """Demo os module functions."""
+    """Demo of os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
 
     # Change to desired directory
@@ -18,8 +17,10 @@ def main():
 
     # Make a new directory
     # The next time you run this, it will crash if the directory exists
-    # TODO: Use exception handling to avoid the crash (just pass)
-    os.mkdir('temp')
+    try:
+        os.mkdir('temp')
+    except FileExistsError:
+        pass
 
     # Loop through each file in the (current) directory
     for filename in os.listdir('.'):
@@ -30,7 +31,6 @@ def main():
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
 
-        # TODO: Try these options one at a time
         # Option 1: rename file to new name - in place
         # os.rename(filename, new_name)
 
@@ -53,8 +53,11 @@ def demo_walk():
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
 
-        # TODO: add a loop to rename the files
+        for filename in filenames:
+            original_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(original_name, new_name)
 
 
-main()
-# demo_walk()
+# main()
+demo_walk()
